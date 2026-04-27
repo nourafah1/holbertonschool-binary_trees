6b1b0365ad09113a25ccbdcd1f48d2ea3fe29025
osama <you@example.com>
2026-04-27 17:52:23 +0300

    Add: Insert node as right-child and reassign existing right-child (Task 2)

--- binary_trees.h
+++ binary_trees.h
@@ -37,5 +37,6 @@ void binary_tree_print(const binary_tree_t *);
 /* Task 0 prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
