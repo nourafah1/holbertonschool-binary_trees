d29310576a5dccfb3a5e7798e2a8afa288f8ee85
osama <you@example.com>
2026-04-27 17:50:42 +0300

    Add: Insert node as left-child and handle existing left-child reassignment (Task 1)

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,6 @@ void binary_tree_print(const binary_tree_t *);
 
 /* Task 0 prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
