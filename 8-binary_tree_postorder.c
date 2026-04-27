948c89d8a47e325405fd24e41268ff5bd6bb993f
osama <you@example.com>
2026-04-27 18:19:45 +0300

    Fix: Refactor post-order traversal documentation for Betty compliance (Task 8)

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,8 +4,6 @@
  * binary_tree_postorder - Goes through a binary tree using post-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
- *
- * Description: If tree or func is NULL, do nothing.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
